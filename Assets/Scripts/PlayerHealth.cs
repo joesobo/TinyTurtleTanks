9dfd97113916826416df5df8c1c4dc73c30fd05b
joesobo <soboleskijoe@gmail.com>
2020-03-01 18:25:25 -0800

    added palyer hleath ui

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,18 +7,28 @@ public class PlayerHealth : MonoBehaviour
     private const int MAXHEALTH = 3;
     [SerializeField]
     private int curHealth;
+    public GameObject curHealthBar;
+    private float barMax = .95f;
+    private float barMin = 0;
 
     void Start()
     {
-        curHealth = MAXHEALTH;        
+        curHealth = MAXHEALTH; 
     }
 
     public void increaseHealth(int amount){
         curHealth += amount;
+        updateHealthBar();
     }
 
     public void decreaseHealth(int amount){
         curHealth -= amount;
+        updateHealthBar();
+    }
+
+    private void updateHealthBar(){
+        float healthPercent = (float)curHealth / (float)MAXHEALTH;
+        curHealthBar.transform.localScale = (new Vector3(barMax * healthPercent + barMin, .8f, 1));
     }
 
     private void Update() {
