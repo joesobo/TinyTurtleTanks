132543fcb4973013108a863ad153374e995748e9
joesobo <soboleskijoe@gmail.com>
2020-03-07 17:25:22 -0800

    started basic pause menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start() {
+        gameObject.transform.localScale = Vector3.zero;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            LeanTween.scale(gameObject, Vector3.one, 0.4f);
+        }
+    }
+
+    public void OnClose(){
+        LeanTween.scale(gameObject, Vector3.zero, 0.4f).setOnComplete(DestroyMe);
+    }
+
+    void DestroyMe(){
+        Destroy(gameObject);
+    }
+}
