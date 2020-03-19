3544198c154f706a55fbe6c98f3a0687c1641cfe
joesobo <soboleskijoe@gmail.com>
2020-03-19 14:05:01 -0700

    new control and quit page

---
+++ Assets/Scripts/UI/ControlLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLoader : MonoBehaviour
+{
+    public void LoadControls(){
+        this.gameObject.SetActive(true);
+    }
+
+    public void UnloadControls(){
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/QuitLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitLoader : MonoBehaviour
+{
+    public void LoadQuit(){
+        this.gameObject.SetActive(true);
+    }
+
+    public void UnloadQuit(){
+        this.gameObject.SetActive(false);
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
