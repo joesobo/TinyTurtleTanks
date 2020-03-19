9e65ac85ba67d055334df60f05275b61b1e32240
joesobo <soboleskijoe@gmail.com>
2020-03-19 12:55:07 -0700

    add new font and credit page

---
+++ Assets/Scripts/UI/CreditLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditLoader : MonoBehaviour
+{
+    public void LoadCredits(){
+        this.gameObject.SetActive(true);
+    }
+
+    public void UnloadCredits(){
+        this.gameObject.SetActive(false);
+    }
+}
