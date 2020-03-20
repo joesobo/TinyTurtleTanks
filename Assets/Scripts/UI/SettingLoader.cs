4a4436b2c71742f33b9a28de62390e8c81996743
joesobo <soboleskijoe@gmail.com>
2020-03-19 17:34:20 -0700

    new visual effects setting button implemented

--- Assets/Scripts/UI/SettingLoader.cs
+++ Assets/Scripts/UI/SettingLoader.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingLoader : MonoBehaviour
 {
+    private GameSettings settings;
+    public Image vfxButton;
+
+    public Color inActiveColor;
+    public Color ActiveColor;
+
+    private void Awake() {
+        settings = FindObjectOfType<GameSettings>();
+    }
+
     public void LoadSettings(){
         this.gameObject.SetActive(true);
     }
 
     public void UnloadSettings(){
         this.gameObject.SetActive(false);
+    }
+
+    public void ChangeVFXSetting(){
+        settings.useVFX = !settings.useVFX;
+        if(settings.useVFX){
+            vfxButton.color = ActiveColor;
+        }else{
+            vfxButton.color = inActiveColor;
+        }
+         
     }
 }
