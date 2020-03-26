81b056b20c4251281a90c3842f520b235737124b
joesobo <soboleskijoe@gmail.com>
2020-03-26 16:33:00 -0700

    Random earth orbiter

---
+++ Assets/Scripts/General/DisplayRotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayRotate : MonoBehaviour
+{
+    public float maxRotationSpeed = 1f;
+    public float minRotationSpeed = 1f;
+    [SerializeField]
+    private float rotateX;
+    [SerializeField]
+    private float rotateY;
+    [SerializeField]
+    private float rotateZ;
+
+    private void Start() {
+        rotateX = GenRandNum(minRotationSpeed, maxRotationSpeed);
+        rotateY = GenRandNum(minRotationSpeed, maxRotationSpeed);
+        rotateZ = GenRandNum(minRotationSpeed, maxRotationSpeed);
+    }
+
+    private void Update() {
+        this.transform.Rotate(rotateX * Time.deltaTime, rotateY * Time.deltaTime, rotateZ * Time.deltaTime);
+    }
+
+    private float GenRandNum(float min, float max){
+        float a = Random.Range(-max, max);
+        if(a < min && a > -min){
+            a = Random.Range(-max, max);
+        }
+        return a;
+    }
+}
