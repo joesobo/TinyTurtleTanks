a703713893f940756051601c95d7450d3b7c7a4f
Joe Soboleski <soboleskijoe@gmail.com>
2020-03-28 18:34:47 -0700

    Better main menu unloading and loading

---
+++ Assets/Scripts/UI/MainMenu/LevelSelectionLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionLoader : MonoBehaviour
+{
+    public void LoadLevelSelection(){
+        this.gameObject.SetActive(true);
+    }
+
+    public void UnloadLevelSelection(){
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/MainLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainLoader : MonoBehaviour
+{
+    public List<GameObject> mainObjects;
+
+    public void LoadMain(){
+        LoadObjects(true);
+    }
+
+    public void UnloadMain(){
+        LoadObjects(false);
+    }
+
+    private void LoadObjects(bool active){
+        foreach (GameObject obj in mainObjects)
+        {
+            obj.SetActive(active);
+        }
+    }
+}
