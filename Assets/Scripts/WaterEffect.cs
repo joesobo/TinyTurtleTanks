29eaffaf4a7dd8fd9f9906079f4e604bf30b28c0
Joe Soboleski <soboleskijoe@gmail.com>
2020-04-22 09:52:43 -0700

    added water effect script

---
+++ Assets/Scripts/WaterEffect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffect : MonoBehaviour
+{
+    public float changeSpeed = 2f;
+    public float changeJump = 50f;
+    public float changeGravity = 0.2f;
+
+    private PlayerController playerController;
+    private SmartEnemy enemyController;
+
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerController.speed -= changeSpeed;
+            playerController.jumpForce -= changeJump;
+        }
+        else if (other.tag == "Enemy")
+        {
+            enemyController = other.gameObject.GetComponent<SmartEnemy>();
+            enemyController.speed -= changeSpeed;
+            //enemyController.jumpForce -= changeJump;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerController.speed += changeSpeed;
+            playerController.jumpForce += changeJump;
+        }
+        else if (other.tag == "Enemy")
+        {
+            enemyController = other.gameObject.GetComponent<SmartEnemy>();
+            enemyController.speed += changeSpeed;
+            //enemyController.jumpForce += changeJump;
+        }
+    }
+}
