1049eb20de741acc9f2c29a401d603519f75629b
Joe Soboleski <soboleskijoe@gmail.com>
2020-05-06 20:38:11 -0700

    Prevent spawned objects from overlapping

--- Assets/Scripts/RaySpawner.cs
+++ Assets/Scripts/RaySpawner.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 //Make it update in edit mode
 //Add edge detection
 //Add cluster spawning
-//Prevent overlapping objects
-[ExecuteInEditMode]
+//Prevent overlapping objects X
+//Add random rotation
 public class RaySpawner : MonoBehaviour
 {
     public List<GameObject> prefabs;
@@ -24,13 +24,9 @@ public class RaySpawner : MonoBehaviour
     public float maxScale = 2;
 
     public LayerMask layerMask;
+    public LayerMask objectMask;
 
     void Start()
-    {
-        ClearObjects();
-    }
-
-    void Update()
     {
         ClearObjects();
         for (int i = 0; i < num; i++)
@@ -43,23 +39,35 @@ public class RaySpawner : MonoBehaviour
     {
         //Cast ray in random direction
         RaycastHit hit = new RaycastHit();
-        while (hit.collider == null)
+        int numberOfCollidersFound = 1;
+        while (hit.collider == null && numberOfCollidersFound != 0)
         {
             //Generate random point in radius and random direction
             Vector3 point = Random.onUnitSphere * radius;
             Vector3 dir = Random.insideUnitCircle.normalized;
             if (Physics.Raycast(point, dir, out hit, checkDst, layerMask))
             {
-                GameObject obj = Instantiate(prefabs[Random.Range(0, prefabs.Count)], hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal), parent);
-                obj.transform.localScale = new Vector3(Random.Range(minScale, maxScale), Random.Range(minScale, maxScale), Random.Range(minScale, maxScale));
-                obj.transform.eulerAngles = new Vector3(transform.eulerAngles.x, Random.Range(0,361), transform.eulerAngles.z);
-                //offset to bring closer to ground
-                obj.transform.position += transform.TransformDirection(-obj.transform.up) * offsetScale * obj.transform.localScale.y;
+                Quaternion normalRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                Vector3 overlapTestBoxScale = Vector3.one;
+                Collider[] collidersInsideOverlapBox = new Collider[1];
+                numberOfCollidersFound = Physics.OverlapBoxNonAlloc(hit.point, overlapTestBoxScale, collidersInsideOverlapBox, normalRotation, objectMask);
+
+                if (numberOfCollidersFound == 0)
+                {
+                    //spawn object
+                    GameObject obj = Instantiate(prefabs[Random.Range(0, prefabs.Count)], hit.point, normalRotation, parent);
+                    //change scale and rotation
+                    obj.transform.localScale = new Vector3(Random.Range(minScale, maxScale), Random.Range(minScale, maxScale), Random.Range(minScale, maxScale));
+                    //obj.transform.eulerAngles = new Vector3(transform.eulerAngles.x, Random.Range(0, 361), transform.eulerAngles.z);
+                    //offset to bring closer to ground
+                    obj.transform.position += transform.TransformDirection(-obj.transform.up) * offsetScale * obj.transform.localScale.y;
+                }
             }
         }
     }
 
-    private void ClearObjects(){
+    private void ClearObjects()
+    {
         foreach (Transform child in parent.transform)
         {
             DestroyImmediate(child.gameObject);
