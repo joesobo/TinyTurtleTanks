a3ee6160fc83c4693435594994751f6a8b9d0910
Joe Soboleski <soboleskijoe@gmail.com>
2020-05-06 19:45:35 -0700

    Created basic ray and mesh spawners

---
+++ Assets/Scripts/MeshSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public MeshFilter meshFilter;
+    public Transform parent;
+
+    public int num;
+    public float scale = 13;
+
+    private Mesh mesh;
+
+    private void Start()
+    {
+        mesh = meshFilter.sharedMesh;
+
+        for (int i = 0; i < num; i++)
+        {
+            GenerateObject();
+        }
+    }
+
+    private void GenerateObject()
+    {
+        int rndTriStart = Random.Range(0, mesh.vertices.Length / 3) * 3; //pick a triangle
+        Vector3 position = mesh.vertices[rndTriStart + 0];
+        Vector3 normal = mesh.normals[rndTriStart + 0];
+
+        Instantiate(prefab, position * scale, Quaternion.FromToRotation(Vector3.up, normal), parent);
+    }
+}
---
+++ Assets/Scripts/RaySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform parent;
+
+    public int num = 10;
+    public int radius = 50;
+    public int checkDst = 5;
+    public float offsetScale = -0.25f;
+
+    public LayerMask layerMask;
+
+    void Start()
+    {
+        for (int i = 0; i < num; i++)
+        {
+            GenerateObject();
+        }
+    }
+
+    private void GenerateObject()
+    {
+        //Cast ray in random direction
+        RaycastHit hit = new RaycastHit();
+        while (hit.collider == null)
+        {
+
+            //Generate random point in radius and random direction
+            Vector3 point = Random.onUnitSphere * radius;
+            Vector3 dir = Random.insideUnitCircle.normalized;
+            if (Physics.Raycast(point, dir, out hit, Mathf.Infinity, layerMask))
+            {
+                //Vector3 pos = hit.point + (Vector3.up * offsetScale);
+                GameObject obj = Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal), parent);
+                obj.transform.position += transform.TransformDirection(-obj.transform.up) * offsetScale;
+            }
+        }
+    }
+}
