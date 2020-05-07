4407e294db1649620e05100cc7a06cddd9705c11
Joe Soboleski <soboleskijoe@gmail.com>
2020-05-06 22:27:08 -0700

    rand rotation, on/off colliders, and topdown spawn

--- Assets/Scripts/RaySpawner.cs
+++ Assets/Scripts/RaySpawner.cs
@@ -11,7 +11,7 @@ public class RaySpawner : MonoBehaviour
     public int radius = 50;
     public int checkDst = 5;
     public float offsetScale = -0.25f;
-    [Range(0,10)]
+    [Range(0, 10)]
     public float maxDensity = 1;
 
     public float minScale = 0.5f;
@@ -20,12 +20,28 @@ public class RaySpawner : MonoBehaviour
     public LayerMask layerMask;
     public LayerMask objectMask;
 
+    public int maxTries = 10;
+    private int currentTries = 0;
+
+    public bool keepColliders = true;
+    public bool spawnTopDown = false;
+    public bool useGizmos = false;
+    public bool useRandomRotation = false;
+
+    public float minHeight = 0;
+    public float maxHeight = 50;
+
     void Start()
     {
         ClearObjects();
         for (int i = 0; i < num; i++)
         {
             GenerateObject();
+        }
+
+        if (!keepColliders)
+        {
+            TurnOffColliders();
         }
     }
 
@@ -36,27 +52,56 @@ public class RaySpawner : MonoBehaviour
         int numberOfCollidersFound = 1;
         while (hit.collider == null && numberOfCollidersFound != 0)
         {
-            //Generate random point in radius and random direction
-            Vector3 point = Random.onUnitSphere * radius;
-            Vector3 dir = Random.insideUnitCircle.normalized;
-            if (Physics.Raycast(point, dir, out hit, checkDst, layerMask))
+            if (currentTries < maxTries)
             {
-                Quaternion normalRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
-                //overlap checking
-                Vector3 overlapTestBoxScale = Vector3.one * maxDensity;
-                Collider[] collidersInsideOverlapBox = new Collider[1];
-                numberOfCollidersFound = Physics.OverlapBoxNonAlloc(hit.point, overlapTestBoxScale, collidersInsideOverlapBox, normalRotation, objectMask);
+                currentTries++;
 
-                if (numberOfCollidersFound == 0)
+                //Generate starting point and direction
+                Vector3 point;
+                Vector3 dir;
+                if (spawnTopDown)
                 {
-                    //spawn object
-                    GameObject obj = Instantiate(prefabs[Random.Range(0, prefabs.Count)], hit.point, normalRotation, parent);
-                    //change scale and rotation
-                    obj.transform.localScale = new Vector3(Random.Range(minScale, maxScale), Random.Range(minScale, maxScale), Random.Range(minScale, maxScale));
-                    //obj.transform.eulerAngles = new Vector3(transform.eulerAngles.x, Random.Range(0, 361), transform.eulerAngles.z);
-                    //offset to bring closer to ground
-                    obj.transform.position += transform.TransformDirection(-obj.transform.up) * offsetScale * obj.transform.localScale.y;
+                    point = Random.onUnitSphere * radius;
+                    dir = -point.normalized;
                 }
+                else
+                {
+                    point = Random.onUnitSphere * Random.Range(minHeight, maxHeight);
+                    dir = Random.insideUnitCircle.normalized;
+                }
+
+                //Start raycast
+                if (Physics.Raycast(point, dir, out hit, checkDst, layerMask))
+                {
+                    Quaternion normalRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+                    //overlap checking
+                    Vector3 overlapTestBoxScale = Vector3.one * maxDensity;
+                    Collider[] collidersInsideOverlapBox = new Collider[1];
+                    numberOfCollidersFound = Physics.OverlapBoxNonAlloc(hit.point, overlapTestBoxScale, collidersInsideOverlapBox, normalRotation, objectMask);
+
+                    //if no overlaps
+                    if (numberOfCollidersFound == 0)
+                    {
+                        //spawn object
+                        GameObject obj = Instantiate(prefabs[Random.Range(0, prefabs.Count)], hit.point, normalRotation, parent);
+                        //change scale and rotation
+                        obj.transform.localScale = new Vector3(Random.Range(minScale, maxScale), Random.Range(minScale, maxScale), Random.Range(minScale, maxScale));
+                        if(useRandomRotation){
+                            obj.transform.Rotate(0, Random.Range(0, 361), 0, Space.Self);
+                        }
+                        //obj.transform.eulerAngles = new Vector3(transform.eulerAngles.x, Random.Range(0, 361), transform.eulerAngles.z);
+                        //offset to bring closer to ground
+                        obj.transform.position += transform.TransformDirection(-obj.transform.up) * offsetScale * obj.transform.localScale.y;
+
+                        currentTries = 0;
+                    }
+                }
+            }
+            else
+            {
+                print("Took too long to find a placement");
+                break;
             }
         }
     }
@@ -66,6 +111,26 @@ public class RaySpawner : MonoBehaviour
         foreach (Transform child in parent.transform)
         {
             DestroyImmediate(child.gameObject);
+        }
+    }
+
+    private void TurnOffColliders()
+    {
+        foreach (Transform child in parent.transform)
+        {
+            Destroy(child.gameObject.GetComponent<BoxCollider>());
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (useGizmos)
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.25f);
+            Gizmos.DrawSphere(Vector3.zero, minHeight);
+
+            Gizmos.color = new Color(0, 0, 1, 0.25f);
+            Gizmos.DrawSphere(Vector3.zero, maxHeight);
         }
     }
 }
