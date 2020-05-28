b7a64d130f8f662b655648592ca5169e8b2877fe
Joe Soboleski <soboleskijoe@gmail.com>
2020-05-28 12:59:25 -0700

    beginning work on boids

---
+++ Assets/Scripts/Boid/BoidController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidController : MonoBehaviour
+{
+    public float minVelocity = 5;
+    public float maxVelocity = 20;
+    public float randomness = 1;
+    public int flockSize = 20;
+    public GameObject prefab;
+    public GameObject chasee;
+
+    public Vector3 flockCenter;
+    public Vector3 flockVelocity;
+
+    private GameObject[] boids;
+
+    public int spawnRadius = 5;
+
+    void Start()
+    {
+        boids = new GameObject[flockSize];
+        for (var i = 0; i < flockSize; i++)
+        {
+            Vector3 position = Random.insideUnitSphere * spawnRadius;
+
+            GameObject boid = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+            boid.transform.parent = transform;
+            boid.transform.localPosition = position;
+            boid.GetComponent<BoidFlocking>().SetController(gameObject);
+            boids[i] = boid;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 theCenter = Vector3.zero;
+        Vector3 theVelocity = Vector3.zero;
+
+        foreach (GameObject boid in boids)
+        {
+            theCenter = theCenter + boid.transform.localPosition;
+            theVelocity = theVelocity + boid.GetComponent<Rigidbody>().velocity;
+        }
+
+        flockCenter = theCenter / (flockSize);
+        flockVelocity = theVelocity / (flockSize);
+    }
+}
