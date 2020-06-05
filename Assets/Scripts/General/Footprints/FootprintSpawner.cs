fbb845a73480b6c268aa862d50d5ff9821cc1d9f
Joe Soboleski <soboleskijoe@gmail.com>
2020-06-05 12:36:26 -0700

    corruption particle and rename conventions

---
+++ Assets/Scripts/General/Footprints/FootprintSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootprintSpawner : MonoBehaviour
+{
+    public GameSettings settings;
+
+    public Vector2 footprintSize = new Vector2(1, 1);
+    public float waitForSeconds = 2;
+    public float offset = 0.1f;
+    public LayerMask terrainLayer;
+    public GameObject footprintPrefab;
+    public GameObject footprintParent;
+    public Color color;
+    [Range(1,30)]
+    public int fadeSpeed = 10;
+
+    private Rigidbody rb;
+
+    private void Start() {
+        settings = FindObjectOfType<GameSettings>();
+
+        if (footprintParent == null) {
+            footprintParent = GameObject.Find("FootprintParent");
+        }
+
+        rb = this.gameObject.GetComponent<Rigidbody>();
+
+        if (settings.useFootPrints) {
+            StartCoroutine("StartSpawn");
+        }
+    }
+
+    IEnumerator StartSpawn() {
+        AddTrailStep();
+        yield return new WaitForSeconds(waitForSeconds);
+        StartCoroutine("StartSpawn");
+    }
+
+    public void AddTrailStep()  {
+        if (rb.velocity.magnitude > 0) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -transform.position, out hit, .75f + .1f, terrainLayer)) {
+                GameObject footprint = Instantiate(footprintPrefab, hit.point, transform.rotation, footprintParent.transform);
+                footprint.transform.Rotate(90, 0, 0);
+                footprint.transform.localScale = footprintSize;
+                footprint.transform.position += hit.normal * offset;
+
+                footprint.GetComponent<Footprint>().color = color;
+                footprint.GetComponent<Footprint>().fadeSpeed = fadeSpeed;
+            }
+        }
+    }
+}
