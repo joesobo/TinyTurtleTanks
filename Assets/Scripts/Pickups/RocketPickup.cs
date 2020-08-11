9548bc9efbcc7252bb7cc0468e7b4c37659e56c4
Joe Soboleski <soboleskijoe@gmail.com>
2020-08-11 13:01:25 -0700

    basic rocket pickup

---
+++ Assets/Scripts/Pickups/RocketPickup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketPickup : Pickup {
+    public Weapon rocket;
+
+    protected override void applyEffect(Collider col) {
+        col.gameObject.GetComponent<PlayerShoot>().weapon = rocket;
+    }
+}
