d30670e0788ad556cbd7a5214b1d0d193aba88a7
Joe Soboleski <soboleskijoe@gmail.com>
2020-08-11 14:05:55 -0700

    basic bomb pickup

---
+++ Assets/Scripts/Pickups/BombPickup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPickup : Pickup {
+    public AltWeapon bomb;
+
+    protected override void applyEffect(Collider col) {
+        col.gameObject.GetComponent<PlayerShoot>().altWeapon = bomb;
+    }
+}
