350d558ed4f9608fd2fac067e6a037b85e52c789
Joe Soboleski <soboleskijoe@gmail.com>
2020-08-21 16:15:12 -0700

    cleaned up trap file

--- Assets/Scripts/Traps/ActivateTrap.cs
+++ Assets/Scripts/Traps/ActivateTrap.cs
@@ -7,8 +7,6 @@ public class ActivateTrap : MonoBehaviour {
     public int moveSpeed = 5;
     private bool moveSpike = false;
     private float maxSpike = -0.25f;
-    private bool blowUp = false;
-    public ParticleSystem explosion;
 
     private GameSettings settings;
 
@@ -18,25 +16,16 @@ public class ActivateTrap : MonoBehaviour {
 
     private void Update() {
         if (!settings.isPaused) {
-            //check if spikes
-            if (spikes != null) {
-                if (moveSpike) {
-                    if (spikes.transform.localPosition.y < maxSpike) {
-                        spikes.transform.localPosition = new Vector3(
-                            spikes.transform.localPosition.x,
-                            spikes.transform.localPosition.y + (moveSpeed * Time.deltaTime),
-                            spikes.transform.localPosition.z);
-                    }
-                }
-            }
-            //otherwise its a mine
-            else {
-                if (blowUp) {
-                    if (settings.useParticle) {
+            AnimateSpikes();
+        }
+    }
 
-                    }
-                }
-            }
+    private void AnimateSpikes() {
+        if (moveSpike && spikes.transform.localPosition.y < maxSpike) {
+            spikes.transform.localPosition = new Vector3(
+                spikes.transform.localPosition.x,
+                spikes.transform.localPosition.y + (moveSpeed * Time.deltaTime),
+                spikes.transform.localPosition.z);
         }
     }
 
@@ -44,7 +33,6 @@ public class ActivateTrap : MonoBehaviour {
         if (col.gameObject.tag == "Player") {
             Debug.Log("Player activated trap");
             moveSpike = true;
-            blowUp = true;
         }
     }
 
