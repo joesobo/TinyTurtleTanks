77c7c7512196b3c3a3e694a592660c802a59cb1a
Joe Soboleski <soboleskijoe@gmail.com>
2020-11-29 18:57:22 -0800

    added explosion sound

---
+++ Assets/AudioAdjuster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAdjuster : MonoBehaviour {
+    private AudioSource source;
+    private GameSettings settings;
+
+    void Start() {
+        source = GetComponent<AudioSource>();
+        settings = FindObjectOfType<GameSettings>();
+
+        source.volume = settings.soundVolume;
+    }
+}
